35431be4b9e760b81cd98e28e0b18dbf1bc8c5e3
slinny <siranli411@gmail.com>
2018-01-13 13:17:41 -0500

    initial commit

---
+++ app/src/main/java/nyc/c4q/mstmusic/Adapter/MusicAdapter.java
@@ -0,0 +1,27 @@
+package nyc.c4q.mstmusic.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import nyc.c4q.mstmusic.Viewholder.MusicViewHolder;
+
+/**
+ * Created by C4Q on 1/13/18.
+ */
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicViewHolder>{
+    @Override
+    public MusicViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(MusicViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
